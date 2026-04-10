eb374b5320d73df648231e6d0ff79e602b20a62b
Aryan Bhalerao <aryan.bhalerao@protonmail.com>
2026-04-10 14:14:48 +0530

    Add files via upload

---
+++ controllers/APIController.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuggyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            string result = "";
+            if (!string.IsNullOrEmpty(result))
+            {
+                return Ok(new { message = "Data fetched" });
+            }
+            return BadRequest("No data");
+        }
+    }
+}
